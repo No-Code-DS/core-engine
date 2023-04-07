75be2b187d8d4f2e806b64170739243c4b5d8fe4
Cine-Pixel <ikaumphriani@gmail.com>
2023-04-07 21:19:54 +0400

    feat(): add login

--- engine/users/router.py
+++ engine/users/router.py
@@ -1,8 +1,34 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi_jwt_auth import AuthJWT
+from sqlalchemy.orm import Session
+from engine.dependencies import get_db
+from engine.users.models import User
+
+from engine.users.schemas import LoginUser
 
 router = APIRouter(prefix="/users")
 
 
-@router.get("/")
-def index():
-    return {"message": "hello from users router"}
+@router.post("/login")
+def login(login_user: LoginUser, Authorize: AuthJWT = Depends(), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email==login_user.email, User.hashed_password==login_user.password).first()
+    if not user:
+        raise HTTPException(status_code=401,detail="Bad username or password")
+    print(user.email)
+    access_token = Authorize.create_access_token(subject=user.email)
+    refresh_token = Authorize.create_refresh_token(subject=user.email)
+    return {"access_token": access_token, "refresh_token": refresh_token}
+
+
+# @router.post("/signup")
+# def sign_up(signup_user: SignupUser, db: Session = Depends(get_db)):
+#     pass
+
+
+@router.post('/refresh')
+def refresh(Authorize: AuthJWT = Depends()):
+    Authorize.jwt_refresh_token_required()
+
+    current_user = Authorize.get_jwt_subject()
+    new_access_token = Authorize.create_access_token(subject=current_user)
+    return {"access_token": new_access_token}
